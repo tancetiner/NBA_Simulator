eb2d2266364a0d1b54c32dcbabfb1411101792fb
Tan Çetiner <cetinertan13@gmail.com>
2022-05-05 01:34:29 +0300

    enchanced the gameplay
    added turnovers, free throws, blocks, steals

--- Game.py
+++ Game.py
@@ -15,42 +15,102 @@ class Game:
         self.score = [0, 0]
         self.stats = Stats(rosters, self.initials)
         self.winner = 0
+        self.overtimeCount = 0
+
+    def gameloop(self, i):
+        pos = rd.randint(1, 110)
+        player = rd.randint(0, 4)
+        name = self.rosters[i % 2][player]
+        if 1 <= pos < 8:
+            player2 = rd.randint(0, 4)
+            name2 = self.rosters[(i + 1) % 2][player2]
+            self.log += self.actionStrings["steal"][rd.randint(0, 1)].format(
+                name2, name
+            )
+        elif 8 <= pos < 11:
+            self.log += self.actionStrings["badPass"][rd.randint(0, 1)].format(
+                name, self.initials[(i + 1) % 2]
+            )
+        elif 11 <= pos < 14:
+            self.log += self.actionStrings["outOfBounds"][rd.randint(0, 1)].format(
+                name, self.initials[(i + 1) % 2]
+            )
+        elif pos == 14:
+            self.log += self.actionStrings["shotClockViolation"][
+                rd.randint(0, 1)
+            ].format(name)
+        elif 15 <= pos < 37:
+            self.score[i % 2] += 2
+            self.log += (
+                self.actionStrings["layupMade"][rd.randint(0, 1)].format(name)
+                + " "
+                + self.scoreString()
+            )
+            self.stats.scores(i % 2, name, 2)
+        elif 37 <= pos < 49:
+            self.log += self.actionStrings["layupMissed"][rd.randint(0, 1)].format(name)
+        elif 49 <= pos < 55:
+            self.score[i % 2] += 2
+            self.log += (
+                self.actionStrings["midrangeMade"][rd.randint(0, 1)].format(name)
+                + " "
+                + self.scoreString()
+            )
+            self.stats.scores(i % 2, name, 2)
+        elif 55 <= pos < 63:
+            self.log += self.actionStrings["midrangeMissed"][rd.randint(0, 1)].format(
+                name
+            )
+        elif 63 <= pos < 75:
+            self.score[i % 2] += 3
+            self.log += (
+                self.actionStrings["3made"][rd.randint(0, 1)].format(name)
+                + " "
+                + self.scoreString()
+            )
+            self.stats.scores(i % 2, name, 3)
+        elif 75 <= pos < 96:
+            self.log += self.actionStrings["3missed"][rd.randint(0, 1)].format(name)
+        elif 96 <= pos < 101:
+            player2 = rd.randint(0, 4)
+            name2 = self.rosters[(i + 1) % 2][player2]
+            self.log += self.actionStrings["shotBlocked"][rd.randint(0, 1)].format(
+                name, name2
+            )
+        else:
+            player2 = rd.randint(0, 4)
+            name2 = self.rosters[(i + 1) % 2][player2]
+            self.log += (
+                self.actionStrings["foul"][rd.randint(0, 1)].format(name, name2) + "\n"
+            )
+            for n in range(2):
+                foul = rd.randint(0, 99)
+                if foul < 77:
+                    self.score[i % 2] += 1
+                    self.log += (
+                        self.actionStrings["freeThrowMade"][n].format(name)
+                        + " "
+                        + self.scoreString()
+                    )
+                    self.stats.scores(i % 2, name, 1)
+                else:
+                    self.log += self.actionStrings["freeThrowMissed"][n].format(name)
+
+                if n == 0:
+                    self.log += "\n"
+
+        self.log += "\n"
+
+        if i % 50 == 0 and i != 0:
+            self.log += "\nEnd of the {}. quarter".format(int(i / 50)) + "\n"
 
     def play(self):
-        overtimeCount = 0
-        self.log += self.initials[0] + " - " + self.initials[1] + "\n"
         for i in range(self.possessionCount):
-            pos = rd.randint(0, 9)
-            player = rd.randint(0, 4)
-            name = self.rosters[i % 2][player]
-            if 0 <= pos <= 4:
-                self.log += name + " " + self.actionStrings["shotMissed"] + "\n"
-            elif 5 <= pos <= 8:
-                self.score[i % 2] += 2
-                self.stats.scores(i % 2, name, 2)
-                self.log += (
-                    name
-                    + " "
-                    + self.actionStrings["shotMade"]
-                    + " "
-                    + self.initials[0]
-                    + " "
-                    + str(self.score[0])
-                    + " - "
-                    + str(self.score[1])
-                    + " "
-                    + self.initials[1]
-                    + "\n"
-                )
-            else:
-                self.log += name + " " + self.actionStrings["shotMissed"] + "\n"
-
-            if i % 50 == 0 and i != 0:
-                self.log += "\nEnd of the {}. quarter".format(int(i / 50)) + "\n"
+            self.gameloop(i)
 
         while self.score[1] == self.score[0]:
-            if overtimeCount != 0:
-                self.log += "Overtime period {} is over!\n".format(overtimeCount)
+            if self.overtimeCount != 0:
+                self.log += "Overtime period {} is over!\n".format(self.overtimeCount)
                 self.log += "Score is {}".format(
                     self.initials[0]
                     + " "
@@ -62,33 +122,10 @@ class Game:
                     + "\n\n"
                 )
 
-            overtimeCount += 1
-            self.log += "Overtime period {} starts!\n".format(overtimeCount)
+            self.overtimeCount += 1
+            self.log += "Overtime period {} starts!\n".format(self.overtimeCount)
             for i in range(16):
-                pos = rd.randint(0, 9)
-                player = rd.randint(0, 4)
-                name = self.rosters[i % 2][player]
-                if 0 <= pos <= 4:
-                    self.log += name + " " + self.actionStrings["shotMissed"] + "\n"
-                elif 5 <= pos <= 8:
-                    self.score[i % 2] += 2
-                    self.stats.scores(i % 2, name, 2)
-                    self.log += (
-                        name
-                        + " "
-                        + self.actionStrings["shotMade"]
-                        + " "
-                        + self.initials[0]
-                        + " "
-                        + str(self.score[0])
-                        + " - "
-                        + str(self.score[1])
-                        + " "
-                        + self.initials[1]
-                        + "\n"
-                    )
-                else:
-                    self.log += name + " " + self.actionStrings["shotMissed"] + "\n"
+                self.gameloop(i)
 
         if self.score[1] > self.score[0]:
             self.winner = 1
@@ -108,6 +145,17 @@ class Game:
         )
 
         self.writeLog()
+
+    def scoreString(self):
+        return (
+            self.initials[0]
+            + " "
+            + str(self.score[0])
+            + " - "
+            + str(self.score[1])
+            + " "
+            + self.initials[1]
+        )
 
     def printLog(self):
         print(self.log)
--- consts.py
+++ consts.py
@@ -3,9 +3,44 @@ from rosters import *
 gameConsts = {
     "numberOfPossessions": 2 * 100,
     "actionStrings": {
-        "shotMade": "shoots... Bang!",
-        "shotMissed": "takes the shot... Misses it",
-        "turnover": "turns the ball over!",
+        "layupMade": ["{} goes for a layup... It's good!", "{} lays the ball in."],
+        "layupMissed": [
+            "{} goes for a layup... No good.",
+            "{} couldn't lay the ball in.",
+        ],
+        "midrangeMade": [
+            "{} from midrange... It's in!",
+            "{} tries from midrange... Count that!",
+        ],
+        "midrangeMissed": [
+            "{} from midrange... It's out.",
+            "{} tries from midrange... Misses it.",
+        ],
+        "3made": ["{} for three... Bang!", "{} takes the three... Count that!"],
+        "3missed": ["{} for three... No good.", "{} takes the three... Misses it."],
+        "shotClockViolation": [
+            "They couldn't shoot the ball! {} ball...",
+            "It's a shot clock violation, great D by {}!",
+        ],
+        "shotBlocked": ["{}'s shot blocked by {}!", "{}'s shot is erased by {}!"],
+        "steal": ["{} steals the ball from {}!", "{} picks {}'s pocket!"],
+        "foul": ["{} is fouled by {}.", "{} gets the foul call. It's on {}."],
+        "badPass": [
+            "Bad pass by {}. {} ball.",
+            "{} throws the ball out! {} possession.",
+        ],
+        "outOfBounds": [
+            "{} steps out of bounds. {} ball.",
+            "{}'s foot is on the line. {} ball.",
+        ],
+        "freeThrowMade": [
+            "First free throw for {}. It's in.",
+            "{} shoots the second one. Makes it.",
+        ],
+        "freeThrowMissed": [
+            "First free throw for {}. It's not good.",
+            "{} shoots the second one. No good.",
+        ],
     },
 }
 
